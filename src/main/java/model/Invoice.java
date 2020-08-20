98907148ae2d4b827bc302e0e3b4300c237fdf6e
Lukasz <lukmyrta@gmail.com>
2020-08-20 20:08:25 +0200

    models created

---
+++ src/main/java/model/Invoice.java
@@ -0,0 +1,71 @@
+package model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "Invoice")
+@Entity
+public class Invoice {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @ApiModelProperty(value = "The id of invoice.", example = "1234", dataType = "Long", position = -1)
+  private final Long id;
+
+  @ApiModelProperty(value = "invoice number", example = "2019/03/2")
+  private final String number;
+
+  @ApiModelProperty(value = "Invoice issue date", example = "2019-03-12")
+  private final LocalDate issuedDate;
+
+  @ApiModelProperty(value = "Invoice due date", example = "2019-04-12")
+  private final LocalDate dueDate;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  private final Company seller;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  private final Company buyer;
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  private final List<InvoiceEntry> entries;
+
+//  @JsonCreator
+//  public Invoice(@JsonProperty("id") Long id,
+//      @JsonProperty("number") String number,
+//      @JsonProperty("issuedDate") LocalDate issuedDate,
+//      @JsonProperty("dueDate") LocalDate dueDate,
+//      @JsonProperty("seller") Company seller,
+//      @JsonProperty("buyer") Company buyer,
+//      @JsonProperty("entries") List<InvoiceEntry> entries) {
+//
+//    this.id = id;
+//    this.number = number;
+//    this.issuedDate = issuedDate;
+//    this.dueDate = dueDate;
+//    this.seller = seller;
+//    this.buyer = buyer;
+//    this.entries = entries != null ? new ArrayList(entries) : new ArrayList();
+//  }
+
+//  private Invoice() {
+//    this.id = null;
+//    this.number = null;
+//    this.issuedDate = null;
+//    this.dueDate = null;
+//    this.seller = null;
+//    this.buyer = null;
+//    this.entries = null;
+//  }
+}
---
+++ src/main/java/model/InvoiceEntry.java
@@ -0,0 +1,74 @@
+package model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@ApiModel(value = "Invoice entry")
+@Entity
+public class InvoiceEntry {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @ApiModelProperty(value = "The id of invoice entry.", dataType = "Long", position = -1)
+  private final Long id;
+
+  @ApiModelProperty(value = "product name", example = "soap")
+  private final String productName;
+
+  @ApiModelProperty(value = "product quantity", example = "2")
+  private final double quantity;
+
+  @ApiModelProperty(value = "unit of quantity ie. kg, pc", example = "1")
+  private final String unit;
+
+  @ApiModelProperty(value = "product net price per unit", example = "29.99")
+  private final BigDecimal price;
+
+  @ApiModelProperty(value = "total net value = (net price per unit)*quantity ", example = "29.99")
+  private final BigDecimal netValue;
+
+  @ApiModelProperty(value = "total gross value (total net value after appropriate VAT rate applied)", example = "39.99")
+  private final BigDecimal grossValue;
+
+  @ApiModelProperty(value = "VAT rate to be applied to net value ie. 0.23 means 23% VAT", example = "VAT_23")
+  private final Vat vatRate;
+
+//  @JsonCreator
+//  public InvoiceEntry(@JsonProperty("id") Long id,
+//      @JsonProperty("productName") String productName,
+//      @JsonProperty("quantity") double quantity,
+//      @JsonProperty("unit") String unit,
+//      @JsonProperty("price") BigDecimal price,
+//      @JsonProperty("netValue") BigDecimal netValue,
+//      @JsonProperty("grossValue") BigDecimal grossValue,
+//      @JsonProperty("vatRate") Vat vatRate) {
+//
+//    this.id = id;
+//
+//    this.productName = productName;
+//    this.quantity = quantity;
+//    this.unit = unit;
+//    this.price = price;
+//    this.netValue = netValue;
+//    this.grossValue = grossValue;
+//    this.vatRate = vatRate;
+//  }
+//
+//  private InvoiceEntry() {
+//    this.id = null;
+//    this.productName = null;
+//    this.quantity = 0;
+//    this.unit = null;
+//    this.price = null;
+//    this.netValue = null;
+//    this.grossValue = null;
+//    this.vatRate = null;
+//  }
+}
