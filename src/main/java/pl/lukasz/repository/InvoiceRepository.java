7991c8e4bfa66eb9460e5c79d9f8f5c4f26d591b
Lukasz <lukmyrta@gmail.com>
2020-08-20 22:04:56 +0200

    InvoiceService added

---
+++ src/main/java/pl/lukasz/repository/InvoiceRepository.java
@@ -0,0 +1,7 @@
+package pl.lukasz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.lukasz.model.Invoice;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+}
