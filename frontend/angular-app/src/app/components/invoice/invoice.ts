93983bba430181c11bd6980d9953ff4ba069cda2
Lukasz <lukmyrta@gmail.com>
2020-08-21 16:25:21 +0200

    model classes added at frontend

---
+++ frontend/angular-app/src/app/components/invoice/invoice.ts
@@ -0,0 +1,37 @@
+export class Invoice {
+  id: number;
+  number: string;
+  issuedDate: Date;
+  dueDate: Date;
+  seller: Company;
+  buyer: Company;
+  invoiceEntries: InvoiceEntry[] = [];
+}
+
+export class InvoiceEntry {
+  id: number;
+  productName: string;
+  quantity: number;
+  unit: string;
+  price: number;
+  netValue: number;
+  grossValue: number;
+  vatRate: vatRate;
+}
+
+export class Company {
+  id: number;
+  name: string;
+  address: string;
+  taxId: string;
+  accountNumber: string;
+  phoneNumber: string;
+  email: string;
+}
+
+export enum vatRate {
+  RATE_,
+  RATE_5,
+  RATE_8,
+  RATE_23
+}
