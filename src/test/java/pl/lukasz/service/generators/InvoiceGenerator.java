02b70c75a5bbecb3a3200b0055d6fee60c858278
Lukasz <lukmyrta@gmail.com>
2020-09-05 08:24:46 +0200

    checkstyle.xml added, build.gradle updated, code style altered to meet checkstyle requirements

--- src/test/java/pl/lukasz/service/generators/InvoiceGenerator.java
+++ src/test/java/pl/lukasz/service/generators/InvoiceGenerator.java
@@ -8,67 +8,67 @@ import pl.lukasz.model.InvoiceEntry;
 
 public class InvoiceGenerator {
 
-    public static Invoice getRandomInvoice() {
-        Long id = IdGenerator.getNextId();
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = LocalDate.now();
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompany();
-        Company buyer = CompanyGenerator.getRandomCompany();
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoice() {
+    Long id = IdGenerator.getNextId();
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = LocalDate.now();
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompany();
+    Company buyer = CompanyGenerator.getRandomCompany();
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
+  }
 
-    public static Invoice getRandomInvoiceWithSpecificBuyerId(Long id) {
-        Long invoiceId = IdGenerator.getNextId();
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = LocalDate.now();
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompany();
-        Company buyer = CompanyGenerator.getRandomCompanyWithSpecificId(id);
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(invoiceId, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoiceWithSpecificBuyerId(Long id) {
+    Long invoiceId = IdGenerator.getNextId();
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = LocalDate.now();
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompany();
+    Company buyer = CompanyGenerator.getRandomCompanyWithSpecificId(id);
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(invoiceId, number, issueDate, dueDate, seller, buyer, entry);
+  }
 
-    public static Invoice getRandomInvoiceWithSpecificSellerId(Long id) {
-        Long invoiceId = IdGenerator.getNextId();
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = LocalDate.now();
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompanyWithSpecificId(id);
-        Company buyer = CompanyGenerator.getRandomCompany();
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(invoiceId, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoiceWithSpecificSellerId(Long id) {
+    Long invoiceId = IdGenerator.getNextId();
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = LocalDate.now();
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompanyWithSpecificId(id);
+    Company buyer = CompanyGenerator.getRandomCompany();
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(invoiceId, number, issueDate, dueDate, seller, buyer, entry);
+  }
 
-    public static Invoice getRandomInvoiceWithoutId() {
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = LocalDate.now();
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompany();
-        Company buyer = CompanyGenerator.getRandomCompany();
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(null, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoiceWithoutId() {
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = LocalDate.now();
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompany();
+    Company buyer = CompanyGenerator.getRandomCompany();
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(null, number, issueDate, dueDate, seller, buyer, entry);
+  }
 
-    public static Invoice getRandomInvoiceWithSpecificId(Long id) {
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = LocalDate.now();
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompany();
-        Company buyer = CompanyGenerator.getRandomCompany();
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoiceWithSpecificId(Long id) {
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = LocalDate.now();
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompany();
+    Company buyer = CompanyGenerator.getRandomCompany();
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
+  }
 
-    public static Invoice getRandomInvoiceWithSpecificIssueDate(LocalDate date) {
-        Long id = IdGenerator.getNextId();
-        String number = InvoiceNumberGenerator.getNextInvoiceNumber();
-        LocalDate issueDate = date;
-        LocalDate dueDate = issueDate.plusDays(2);
-        Company seller = CompanyGenerator.getRandomCompany();
-        Company buyer = CompanyGenerator.getRandomCompany();
-        List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
-        return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
-    }
+  public static Invoice getRandomInvoiceWithSpecificIssueDate(LocalDate date) {
+    Long id = IdGenerator.getNextId();
+    String number = InvoiceNumberGenerator.getNextInvoiceNumber();
+    LocalDate issueDate = date;
+    LocalDate dueDate = issueDate.plusDays(2);
+    Company seller = CompanyGenerator.getRandomCompany();
+    Company buyer = CompanyGenerator.getRandomCompany();
+    List<InvoiceEntry> entry = InvoiceEntryGenerator.getRandomEntries(2);
+    return new Invoice(id, number, issueDate, dueDate, seller, buyer, entry);
+  }
 }
